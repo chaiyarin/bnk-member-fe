1aaf6fa56e54f3e8a77ecf74ca469e6855c04b21
Chaiyarin Niamsuwan <chaiyarin@odds.team>
2018-07-21 17:51:02 +0700

    add api_url in environment production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  api_url: 'https://bnk-nodejs-express-api.herokuapp.com',
+  instagram_api_url: 'https://www.api.bnk48.com/api/social-feeds?page=1&max=1000000000000000&limit=19&username='
 };
